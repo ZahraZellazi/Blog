5feac6a3359543ca23c314c71ea56d774bf92741
Zahra Zellazi <101278694+ZahraZellazi@users.noreply.github.com>
2024-07-17 21:33:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import Header from './components/Header/Header'
-
+import React, { useState, useEffect } from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import BlogGrid from './Components/BlogGrid';
 
 function App() {
-  
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <Header/>
-  )
+    <React.Fragment>
+      {windowWidth <= 770 ? (
+        <>
+          <Navbar />
+          <BlogGrid />
+        </>
+      ) : (
+        <>
+          <Header />
+          <BlogGrid />
+        </>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
